64b2f5a69b490c1f11d846f25187f99f5fedc0f1
SandeepTENGALE <sandeep.tengale@in.ibm.com>
2019-03-30 01:52:56 +0530

    Add bootstrap and jquery supporyt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
