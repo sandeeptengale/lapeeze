64aaf3cbd302af19a482220eda2f6ddb716f466a
SandeepTENGALE <sandeep.tengale@in.ibm.com>
2019-03-30 14:41:06 +0530

    Navbar routes are working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { EventsComponent } from './events/events.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent
+  }, {
+    path: 'contact', component: ContactComponent
+  }, {
+    path: 'services', component: ServicesComponent
+  }, {
+    path: 'events', component: EventsComponent
+  }, {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }, {
+    path: '**',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [HomeComponent, ContactComponent, ServicesComponent, EventsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
 import { NavbarComponent } from './navbar/navbar.component';
+import { Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
